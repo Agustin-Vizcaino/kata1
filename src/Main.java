5de3abea2652b5a1183ff43a21c071b22f75952c
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-01-17 19:58:36 +0000

    Import java.util.Random, added random selection of fate

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Random;
+
 public class Main {
     static String[] fates;
+    static Random random;
 
     static {
         fates = new String[] { "luck", "wealth", "misfortune" };
+        random = new Random();
     }
 
     public static void main(String[] args) {
-        System.out.println(fates[1]);
+        System.out.println(fates[random.nextInt(fates.length)]);
     }
 }
