bdf81bcb6bba849f8bccbee624d2806652a3a659
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-01-17 19:43:15 +0000

    Added data structure for fates and test in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    static String[] fates;
+
+    static {
+        fates = new String[] { "luck", "wealth", "misfortune" };
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(fates[1]);
     }
 }
