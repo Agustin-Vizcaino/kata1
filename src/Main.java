2b449ed324573ccb3b2409ccc10ac57216fca781
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-01-17 20:59:27 +0000

    Polished code, added full welcome message

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,15 @@ import java.util.Random;
 public class Main {
     static String[] fates;
     static Random random;
+    static int len;
 
     static {
         fates = new String[] { "luck", "wealth", "misfortune" };
+        len = fates.length;
         random = new Random();
     }
 
     public static void main(String[] args) {
-        System.out.println(fates[random.nextInt(fates.length)]);
+        System.out.println("Behold! Your fate for today is, as revealed by the Digital 8-Ball: " + fates[random.nextInt(len)]);
     }
 }
